8c3edd27025f91faf5b38158dd3bea451e326423
Surajit Roy <82498991+Surajit-50@users.noreply.github.com>
2021-10-27 12:59:08 +0530

    Delete js1.js

--- js1.js
+++
@@ -1,8 +0,0 @@
-
-function myFunction() {
-    let x = document.getElementById("numb").value;
-    let ob=new Date();
-    let y=ob.getFullYear();
-
-    document.getElementById("demo").innerHTML = y-x+"years";
-  }
